aa04adc97258389ebb4bd585640767bab4c243fd
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-10-04 19:51:18 -0600

    Finished the knowledge check with requests that takes arguments

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from dbhelpers import run_statement
 import json
 
@@ -7,6 +7,17 @@ app = Flask(__name__)
 @app.get('/api/clients')
 def get_all_clients():
     results = run_statement("CALL get_all_clients()")
+    if(type(results) == list):
+        clients_json = json.dumps(results, default=str)
+        return clients_json
+    else:
+        return "Sorry, something has gone wrong."
+
+
+@app.get('/api/loyal_clients')
+def get_clients_by_loyalty_points():
+    min_price = request.args.get('min_price')
+    results = run_statement("CALL get_clients_by_loyalty_points(?)", [min_price])
     if(type(results) == list):
         clients_json = json.dumps(results, default=str)
         return clients_json
--- flask_endpoints.sql
+++ flask_endpoints.sql
@@ -66,6 +66,27 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `get_clients_by_loyalty_points` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_clients_by_loyalty_points`(loyalty_points_input int unsigned)
+begin
+	select c.username, c.loyalty_points, c.joined_on 
+	from clients c
+	where c.loyalty_points > loyalty_points_input;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -76,4 +97,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-04 19:14:09
+-- Dump completed on 2022-10-04 19:50:50
